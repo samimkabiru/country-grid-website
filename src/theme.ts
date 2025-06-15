39c4588cbdd976873d38714ec69a9826ac5531d3
Samim-kabiru <samimkabiru@gmail.com>
2025-06-15 07:52:07 +0100

    Customize the theme to get lighter grays.

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,20 @@ const config: ThemeConfig = {
 
 const theme = extendTheme({
   config,
+  colors: {
+    gray: {
+      50: '#e9f4fa',
+      100: '#cfdae2',
+      200: '#b2c2cd',
+      300: '#93a9b8',
+      400: '#7690a4',
+      500: '#5c778a',
+      600: '#475d6d',
+      700: '#33434e',
+      800: '#1d2830',
+      900: '#050e15',
+    },
+  },
 });
 
 export default theme;
